cd1d9094ccabebb51cfce997b46da2694703c4fc
Aovthz <47479122+Mandaranian-Cactus@users.noreply.github.com>
2020-04-11 09:54:04 -0400

    Update 2D Example Terrain Generation (Only Height).py

--- 2D Example Terrain Generation (Only Height).py
+++ 2D Example Terrain Generation (Only Height).py
@@ -14,25 +14,22 @@ class Window:
         self.screen.fill((255, 255, 255))
 
 
-def fade(t):  # Fade function to smooth out rigidness
-    return 6 * t ** 5-15 * t ** 4+10 * t ** 3
-
 def add_octaves(x, y, octaves, persistences, frequencies):  # Here, octaves/perlin noises are combined together
     total = 0  # Total of the accumulated sums of the octaves
-    max_value = 0   # Used to help the output be between 0 - 1 (Combined with later code)
+    max_value = 0   # Used to help the output be within a given range (Range is chosen based on the input data. If the range of input were from -1 to 1, this variable helps mataining that same rangeat output)
     for i in range(octaves):
         persistence = persistences[i]  # Persistence regulates how much an octave affects the final overall outcome
         frequency = frequencies[i]  # Frequency dictates the gap between points on an octave (Smaller gaps have smoother curves while larger gaps have more rigid curves)
         n = Noise(x * frequency, y * frequency)
         total += n.return_val() * persistence
         max_value += persistence
-    return total / max_value
+    return total / max_value  # "max_value" is used to keep within input range
 
 
 screen = Window(700, 525)
 pygame.init()
 clock = pygame.time.Clock()
-noise_move_increment = 0.025317
+noise_move_increment = 0.025317  # How much we move each time on the perlin noise (Smaller = smoother, larger = more chaotic)
 persistences = [10, 2, 1, 3]
 frequencies = [0.5, 2, 1, 0.5]
 
@@ -45,20 +42,21 @@ for y in range(h):
     for x in range(w):
         output = add_octaves(x_pos, y_pos, len(persistences), persistences,frequencies)
         output = (output + 1) / 2  # Set within the range from 0 - 1
-        output = output ** 2  # Exponential add (This adjusts the range of the output from 0 - 2 to 0 - 2 ** expoenent)
+        output = output ** 2  # Exponential function used to create more rigidness and variation (Leads to more ocean and mountains)
 
+        # Limits for each terrain type (Water, Mountain, forest, etc.)
         if output < 0.1:
-            color = (0,0,175)  # Water
+            color = (0, 0, 175)  # Water
         elif output < 0.12:
             color = (194, 178, 128)  # Beach
         elif output < 0.25:
             color = (34,139,34)  # Grass
         elif output < 0.35:
-            color = ((0,100,0))  # Forest
+            color = (0, 100, 0)  # Forest
         elif output < 0.5:
             color = (211, 211, 211)  # Mountain
         else:
-            color = (255, 255, 255)
+            color = (255, 255, 255)  # Snow
 
         screen.screen.set_at((x, y), color)
         x_pos += noise_move_increment
@@ -70,5 +68,4 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    # pygame.display.update()
     clock.tick(50)  # Fps (Don't know why/how it does it)
